75a0e01638cdeb1fb8e0687597666c1b65f9db97
Eric Nguyen <enguyen807@gmail.com>
2023-10-20 09:31:08 +0300

    Make items dynamic

--- src/App.js
+++ src/App.js
@@ -17,7 +17,12 @@ function App() {
       amount: 94.12,
       date: new Date(2020, 7, 14),
     },
-    { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
+    { 
+      id: 'e2', 
+      title: 'New TV', 
+      amount: 799.49, 
+      date: new Date(2021, 2, 12) 
+    },
     {
       id: 'e3',
       title: 'Car Insurance',
@@ -34,8 +39,7 @@ function App() {
 
   return (
     <div>
-      <h2>Let's get started!</h2>
-      <Expenses item={expenses}/>
+      <Expenses items={expenses}/>
     </div>
   );
 }
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -2,10 +2,14 @@ import "./Expenses.css";
 import ExpenseItem from "../ExpenseItem/ExpenseItem";
 import Card from '../Card/Card';
 
-function Expenses({ item }) {
+function Expenses({ items }) {
     return (
         <Card className="expenses">
-            <ExpenseItem title={item[0].title} amount={item[0].amount} date={item[0].date}/>
+            {
+                items.map((item) => (
+                    <ExpenseItem title={item.title} amount={item.amount} date={item.date}/>
+                ))
+            }
         </Card>
     )
 }
